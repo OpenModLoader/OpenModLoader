d67cb2aaf12527a7a8fc58e0c85857533e2eeaf4
gegy1000 <gegy1000@gmail.com>
2018-10-28 15:55:06 +0200

    Merge branch 'master' into mod-rewrite
    # Conflicts:
    #	src/main/java/com/openmodloader/loader/mixin/OMLMixinProvider.java

--- src/main/java/com/openmodloader/loader/mixin/OMLMixinProvider.java
+++ src/main/java/com/openmodloader/loader/mixin/OMLMixinProvider.java
@@ -5,43 +5,22 @@ import me.modmuss50.fusion.api.IMixinProvider;
 import java.util.Map;
 
 public class OMLMixinProvider implements IMixinProvider {
-	@Override
-	public Class[] getMixins(Map<String, String> environmentData) {
-		if (environmentData.get("side").equalsIgnoreCase("server")) {
-			return new Class[]{
-					MixinCommandManager.class,
-					MixinDedicatedServer.class,
-					MixinMinecraftServer.class,
-					//Registry
-					MixinBiome.class,
-					MixinBlock.class,
-					MixinEnchantment.class,
-					MixinItem.class,
-					MixinPotion.class
-			};
-		}
-		return new Class[]{
-<<<<<<< HEAD
-			MixinClientBrand.class,
-			MixinCommandManager.class,
-			MixinDedicatedServer.class,
-			MixinGuiScreen.class,
-			MixinMinecraftClient.class,
-			MixinMinecraftServer.class,
-=======
-				MixinClientBrand.class,
-				MixinGuiScreen.class,
-				MixinMinecraftClient.class,
-				MixinCommandManager.class,
-				MixinDedicatedServer.class,
-				MixinMinecraftServer.class,
-				//Registry
-				MixinBiome.class,
-				MixinBlock.class,
-				MixinEnchantment.class,
-				MixinItem.class,
-				MixinPotion.class
->>>>>>> master
-		};
-	}
+    @Override
+    public Class[] getMixins(Map<String, String> environmentData) {
+        if (environmentData.get("side").equalsIgnoreCase("server")) {
+            return new Class[] {
+                    MixinCommandManager.class,
+                    MixinDedicatedServer.class,
+                    MixinMinecraftServer.class,
+            };
+        }
+        return new Class[] {
+                MixinClientBrand.class,
+                MixinGuiScreen.class,
+                MixinMinecraftClient.class,
+                MixinCommandManager.class,
+                MixinDedicatedServer.class,
+                MixinMinecraftServer.class,
+        };
+    }
 }
