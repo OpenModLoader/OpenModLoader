7b1b7f93ff3a9af8dc939ef8023c702230ea94b1
gegy1000 <gegy1000@gmail.com>
2018-11-17 17:11:38 +0200

    18w46a

--- src/main/java/com/openmodloader/loader/BuiltinModReporter.java
+++ src/main/java/com/openmodloader/loader/BuiltinModReporter.java
@@ -9,7 +9,7 @@ import com.openmodloader.api.mod.config.VoidModConfigurator;
 public class BuiltinModReporter implements IModReporter {
     @Override
     public void apply(ModReportCollector collector, ModConstructor constructor) {
-        ModMetadata vanillaMetadata = new ModMetadata("minecraft", Version.valueOf("1.14.0+18w43b"));
+        ModMetadata vanillaMetadata = new ModMetadata("minecraft", Version.valueOf("1.14.0+18w46a"));
         collector.report(new ModCandidate(vanillaMetadata, new VoidModConfigurator()).global());
 
         ModMetadata omlMetadata = new ModMetadata("openmodloader", OpenModLoader.VERSION);
